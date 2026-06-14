86497c4b41ecbaaf574801ab66b54dc79a678d44
Kish210 <admin@samanarm.ir>
2026-06-14 11:41:48 +0330

    feat(OPT-11): فوکوسِ خودکارِ نوارِ بارکدِ POS هنگام باز شدن (اسکن پیوسته)

--- src/SamaHesab.WPF/Views/POS/PosView.xaml.cs
+++ src/SamaHesab.WPF/Views/POS/PosView.xaml.cs
@@ -5,7 +5,12 @@ namespace SamaHesab.WPF.Views.POS;
 
 public partial class PosView : UserControl
 {
-    public PosView() => InitializeComponent();
+    public PosView()
+    {
+        InitializeComponent();
+        // OPT-11: فوکوسِ همیشگیِ نوارِ بارکد برای اسکنِ پیوسته و سریع
+        Loaded += (_, _) => BarcodeBox.Focus();
+    }
 
     private void ServerSettings_Click(object sender, RoutedEventArgs e)
     {
